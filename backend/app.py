063bc1de838fcee9aeb11a540cce575003eae9fb
cfp-rbg <cfp4@hood.edu>
2024-11-05 12:29:03 -0500

    API Integration and Base Frontend

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def get_movies():
-        api_key='f05530ca'
+api_key = 'f05530ca'
 
-        response = requests.get(f'https://www.omdbapi.com/?i=tt3896198&apikey=f05530ca')
-        return jsonify(response.json())
+@app.route('/get-movie', methods=['GET'])
+def get_movie():
+        movie_name = request.args.get('title')
+
+        if not movie_name:
+                return jsonify({"error": "Movie's title was not provided."}), 400
+        
+        response = requests.get(f'http://www.omdbapi.com/?t={movie_name}&apikey={api_key}')
+        data = response.json()
+
+        if data.get('Response') == 'True':
+                movie_title = data['Title']
+                movie_plot = data['Plot']
+                return jsonify({"title": movie_title, "plot": movie_plot})
+        else:
+                return jsonify({"error": "The movie was not found. Please check to make sure you provided the correct spelling."}), 404
+
 
 if __name__ == '__main__':
         app.run(debug=True)
