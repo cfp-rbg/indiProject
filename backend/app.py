b95a53fb594e97a7aa1d217db15bbf54c4168b6e
cfp-rbg <cfp4@hood.edu>
2024-11-12 15:14:11 -0500

    Final

--- backend/app.py
+++ backend/app.py
@@ -1,67 +1,130 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import requests, string
+from data.dicts import genres
 
 app = Flask(__name__)
+CORS(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize SQLAlchemy
+db = SQLAlchemy(app)
+
+#store api key
 api_key = 'f05530ca'
+
+def get_data(movie_title, api_key):
+       response = requests.get(f'http://www.omdbapi.com/?t={movie_title}&apikey={api_key}')
+       if response.status_code == 200:
+              return response.json()
+       else:
+              return None
 
 @app.route('/get-movie', methods=['GET'])
 def get_movie():
         movie_name = request.args.get('title')
 
         if not movie_name:
-                return jsonify({"error": "Movie's title was not provided."}), 400
+               return jsonify({"error": "No movie title given."}), 400
+
+        movie = read_movie(movie_name)
+
+        if movie: #If found in db, recalculate genres and update in database
+               predictedGenres = genrePredict(movie.plot)
+               update_movie(movie.id, predictedGenres)
+
+               return jsonify({
+                      "Title": movie.title,
+                      "Plot": movie.plot,
+                      "Predicted Genres": movie.predicted_genres
+                      })
         
-        response = requests.get(f'http://www.omdbapi.com/?t={movie_name}&apikey={api_key}')
-        data = response.json()
+        #store api data into database, return it to frontend
+        data = get_data(movie_name, api_key)
+        if data and data.get('Response') == 'True':
+                title = data['Title']
+                plot = data['Plot']
+                predictedGenres = genrePredict(plot)
 
-        if data.get('Response') == 'True':
-                movie_title = data['Title']
-                movie_plot = data['Plot']
-
-                predictedGenres = genrePredict(movie_plot)
-                return jsonify({
-                        "title": movie_title,
-                        "plot": movie_plot,
-                        "predicted_genres": predictedGenres[:3] #specifically show top three, if there are more than 3 genres with scores greater than 0
-                        })
-        else:
+                add_movie(data)
+                movie = read_movie(title)
+                if movie:
+                       update_movie(movie.id, predictedGenres)
+                       return jsonify({
+                              "Title": movie.title,
+                              "Plot": movie.plot,
+                              "Predicted Genres": movie.predicted_genres
+                       })
+                
+        else: #If entered movie title does not match any movie in the API
                 return jsonify({"error": "The movie was not found. Please check to make sure you provided the correct spelling."}), 404
 
-
+#genre prediction algorithm
 def genrePredict(movie_plot):
-
-        #genre dictionaries
-        genres = {
-                "Horror": ["blood", "death", "dead", "kill", "killer", "bloody", "deadly", "weapon", "murder", "scare", "scary", "monster", "monsters", "scream", "screams", "screaming", "clown", "fear", "haunted", "demon", "demonic", "preys", "prey", "possess", "possesses", "entity", "victims", "victim", "murder", "slay", "slay", "slaughter", "slaughters", "assassinate", "massacre", "suspense", "tension", "mystery", "thriller", "killing", "killing spree", "occult"],
-                "Comedy": ["funny", "laugh", "laughs", "joke", "jokes", "humor", "parody", "parodies", "comedy", "clown", "misadventures", "slapstick", "sarcastic", "absurd", "ironic", "mock", "spoof", "satire", "lighthearted", "quirky"],
-                "Romance": ["love", "loving", "heart", "lovers", "romance", "romantic", "kiss", "couple", "couples", "affection", "date", "dating", "marry", "marriage", "partner", "wife", "husband", "ex", "girlfriend", "boyfriend", "divorce", "struggle", "relationship", "commitment", "longing", "passion", "heartbreak", "unrequited"],
-                "Action": ["fight", "fights", "explosion", "explosions", "hero", "heroes", "battle", "war", "mission", "save", "doom", "disaster", "combat", "battle", "duel", "warfare", "confrontation", "disaster", "chase", "gun", "gunfight", "violence", "rescue", "adventure", "bravery", "thriller", "danger", "climax", "superhero", "saving", "villain", "villains"],
-                "Fantasy": ["monster", "beast", "vampire", "vampires", "werewolf", "werewolves", "dragon", "dragons", "unicorn", "unicorns", "magic", "prince", "princess", "king", "queen", "wizard", "witch", "enchantment", "spell", "myth", "fairy", "elf", "dwarf", "goblin", "quest", "villain", "villains"],
-                "Sci-fi": ["alien", "space", "spaceship", "ship", "robot", "future", "technology", "intergalactic", "extraterrestrial", "spacecraft", "laser", "galaxy", "celestial", "futuristic", "cyberpunk", "virtual", "future", "cybernetics", "virtual reality", "android", "nanotechnology", "artificial intelligence", "machine", "hologram", "dystopia", "post-apocalyptic", "terraforming", "bioengineering", "superhero"],
-                "Mystery": ["murder", "murderer", "strange", "killed", "crime", "crimes", "weapon", "weapons", "mystery", "mysteries", "gun", "knife", "poison", "suspense", "tension", "mystery", "thriller", "detective", "investigation", "clue", "suspense", "secret", "unexplained", "whodunit", "enigma", "thriller", "conspiracy", "hidden", "identify", "case", "investigate", "investigates", "sleuth"],
-                }
-        
         #set up genre scores
         scores = {genre: 0 for genre in genres}
 
         #take plot summary, remove capitalization and split into list
         plotSumm = movie_plot.translate(str.maketrans('', '', string.punctuation)).lower().split()
 
-        #Iterate through 
+        #Iterate through plo, 
         for word in plotSumm:
                 for genre, keyword_list in genres.items():
                         if word in keyword_list:
                                 scores[genre]+=1
 
+        #If all scores are 0, return string
+        if all(score == 0 for score in scores.values()):
+               return 0
+        
         #Only show scores greater than 0
         scores = {genre: score for genre, score in scores.items() if score > 0}
 
         #Order the scores from greatest to least
         orderedScores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
 
-        return orderedScores;
+        predictedGenres = {genre: score for genre, score in orderedScores}
+        return predictedGenres;
+        
 
+class Movie(db.Model):
+    __tablename__ = 'movie_data'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+    plot = db.Column(db.Text, nullable=False)
+    predicted_genres = db.Column(db.JSON, nullable=True)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
+    
+def add_movie(api_data):
+       if api_data: #check if movie already exists in database. if not, add it
+              existing = Movie.query.filter_by(title=api_data['Title']).first()
+              
+              if not existing:
+                     movie = Movie(
+                            title = api_data['Title'],
+                            plot = api_data['Plot'],
+                            predicted_genres = None)
+                     db.session.add(movie)
+                     db.session.commit()
+
+def read_movie(title): #get and return movie data from database
+       movie = Movie.query.filter_by(title=title).first()
+       return movie
+
+def update_movie(movie_id, predicted_genres): #specifically update movie genres, since they're the only thing being changed
+       movie = Movie.query.get(movie_id)
+       if movie:
+              movie.predicted_genres = predicted_genres
+              db.session.commit()
+              return movie
+       return None
 
 if __name__ == '__main__':
+        with app.app_context():
+               db.create_all()
         app.run(debug=True)
--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Plot from 'react-plotly.js';
 
 function App() {
+  const [movieName, setMovieName] = useState('')
+  const [movieData, setMovieData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  //handle user movie name submissions
+  const handleSearch = async(movieName) => {
+    if(!movieName) return
+    setLoading(true)
+    setError('')
+  
+  //Interact with backend to grab data
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/get-movie?title=${movieName}`)
+      const data = await response.json()
+
+      if(response.ok) {
+        setMovieData(data)
+      } else {
+        setError(data.error) //display error message if entered movie isn't found
+      }
+      setLoading(false)
+
+    } catch(err) { //if frontend cannot pull movie data from backend, return error
+      setLoading(false)
+      setError("Could not get media data.")
+    }
+  };
+
+  //create pie chart based on returned genre scores
+  function Graph({predictedGenres}) {
+
+    if(!predictedGenres|| Object.keys(predictedGenres).length === 0) { //if all scores = 0, display error
+      return <p>There is not enough data in the plot to predict genres.</p>;
+    }
+
+    const genres = Object.keys(predictedGenres)
+    const scores = Object.values(predictedGenres)
+
+    return (
+      <Plot
+        data = {[
+          {
+            type: 'pie',
+            labels: genres,
+            values: scores,
+            textinfo: 'none',
+            hoverinfo: 'label+scores+percent',
+          },
+        ]}
+        layout = {{
+          title: 'Genre Probabilities',
+          showlegend:true,
+        }}
+      />
+    );
+  }
+  
+  //display everything
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h1>Media Genre Guesser</h1>
+          <input
+            type = "text"
+            value = {movieName}
+            onChange = {(e) => setMovieName(e.target.value)}
+            placeholder = "Enter media title"
+          />
+
+          <button onClick = {() => handleSearch(movieName)}>Search</button>
+
+          {loading && <p>Loading...</p>}
+          {error && <p>{error}</p>}
+
+          {
+            movieData && (
+              <div>
+                <h2>{movieData.Title}</h2>
+                <p>Plot: {movieData.Plot}</p>
+
+                <Graph
+                  predictedGenres = {movieData['Predicted Genres']}
+                />
+              </div>
+            )
+          }
+      </div>
   );
 }
+
 
 export default App;
